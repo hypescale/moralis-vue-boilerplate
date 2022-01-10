63adbdc998a76482dc9ae3d8cb644c5afc99ff47
Martin Kogut <mail@hypescale.com>
2022-01-10 22:47:59 +0100

    feat: improved login

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -1,21 +1,36 @@
 <template>
   <div>{{user}}</div>
-  <button v-if="!user" @click="logIn">logIn</button>
-  <button v-if="user" @click="logOut">LogOut</button>
+  <button v-if="!isAuthenticated" @click="logIn">logIn</button>
+  <button v-if="isAuthenticated" @click="logOut">LogOut</button>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import Moralis from 'moralis'
 
-Moralis.initialize(process.env.VUE_APP_MORALIS_APPLICATION_ID);
-Moralis.serverURL = process.env.VUE_APP_MORALIS_SERVER_URL;
-const user = ref(null)
+const serverUrl = process.env.VUE_APP_MORALIS_SERVER_URL
+const appId =  process.env.VUE_APP_MORALIS_APPLICATION_ID
+Moralis.start({ serverUrl, appId })
+
+let user = ref({})
+
+onMounted(() => {
+  user.value = Moralis.User.current();
+})
+
+const isAuthenticated = computed(() => user.value)
 
 const logIn = async () => {
-  user.value = Moralis.User.current();
-  if (!user.value) {
-    user.value = await Moralis.authenticate();
+  if (!isAuthenticated.value) {
+    user.value = await Moralis.authenticate({ signingMessage: "Log in using Moralis" })
+      .then((user) => {
+        console.log("logged in user:", user);
+        console.log(user.get("ethAddress"));
+        return user
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
   console.log("logged in user:", user.value);
 }
